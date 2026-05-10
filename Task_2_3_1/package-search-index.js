eea632bf4a5f105c1279f5fa641ded8339083dbd
Wa1ker1 <Wa1ker1@users.noreply.github.com>
2026-05-10 13:52:21 +0000

    Deploying to gh-pages from @ Wa1ker1/OOP@25cfe7a881064c3c517ded83928e986a74511bb9 🚀

--- Task_2_3_1/package-search-index.js
+++ Task_2_3_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.filippova"},{"l":"ru.nsu.filippova.controller"},{"l":"ru.nsu.filippova.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.filippova"},{"l":"ru.nsu.filippova.controller"},{"l":"ru.nsu.filippova.model"},{"l":"ru.nsu.filippova.view"}];updateSearchResults();
